a4cce7218282eb485d6ab37e5f67832d304e2211
Futa Nagashima <aruimimiura0@gmail.com>
2019-06-01 20:53:52 +0900

    heroku setting

--- isaki/settings/heroku.py
+++ isaki/settings/heroku.py
@@ -1,14 +1,15 @@
 from .common import *
 
+import dj_database_url
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-    }
-}
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FOWARDED_PROTO', 'https')
