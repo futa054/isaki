aee0244140fe76f0ca2faccab83f1ee815daadc0
Futa Nagashima <aruimimiura0@gmail.com>
2019-06-01 21:20:05 +0900

    heroku settings mod2

--- isaki/settings/heroku.py
+++ isaki/settings/heroku.py
@@ -3,13 +3,12 @@ from .common import *
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if os.environ.get('debug', False):
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES['default'] = dj_database_url.config()
-
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- isaki/wsgi.py
+++ isaki/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 from whitenoise.django import DjangoWhiteNoise
-
+ 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "isaki.settings.heroku")
  
 application = get_wsgi_application()
