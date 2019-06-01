dd2c58a2a9231335d8e45aca4b0e7e10f4764712
Futa Nagashima <aruimimiura0@gmail.com>
2019-06-01 20:41:16 +0900

    wsgi mod

--- isaki/wsgi.py
+++ isaki/wsgi.py
@@ -2,6 +2,6 @@ import os
  
 from django.core.wsgi import get_wsgi_application
  
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "microblog.settings.heroku")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "isaki.settings.heroku")
  
 application = get_wsgi_application()
