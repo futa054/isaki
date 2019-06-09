3b9a51e0d8c986f15d797494d1f72d1df29431d4
Futa Nagashima <aruimimiura0@gmail.com>
2019-06-10 02:35:06 +0900

    add cloudinary quality setting

--- templatetags/util_tag.py
+++ templatetags/util_tag.py
@@ -3,4 +3,4 @@ register = template.Library()
 
 @register.filter(name="formatImageUrl")
 def formatImageUrl(value):
-    return value.replace('image/upload/', 'image/upload/f_auto/')
+    return value.replace('image/upload/', 'image/upload/f_auto,q_auto/')
