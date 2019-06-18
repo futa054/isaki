34ce98f01a53a338eaaff05edac821d246282bad
Futa Nagashima <aruimimiura0@gmail.com>
2019-06-18 22:16:09 +0900

    add cloudinary setting

--- isaki/settings/heroku.py
+++ isaki/settings/heroku.py
@@ -25,3 +25,11 @@ INSTALLED_APPS = [
     'cloudinary',
     'cloudinary_storage',
 ]
+
+CLOUDINARY_STORAGE = {
+'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
+'API_KEY': env('CLOUDINARY_API_KEY'),
+'API_SECRET': env('CLOUDINARY_API_SECRET')
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
